dbb8119a5dfa44a72145c7a782d6e53465cd1e54
Bruno Costa <brn.98@live.com>
2023-08-01 09:33:39 -0300

    Pequenas mudanças

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -6,6 +6,7 @@ use yii\bootstrap5\Breadcrumbs;
 use yii\web\YiiAsset;
 use yii\bootstrap5\BootstrapPluginAsset;
 
+
 YiiAsset::register($this);
 BootstrapPluginAsset::register($this);
 AppAsset::register($this);
@@ -38,38 +39,6 @@ $this->beginPage();
     </main>
 
     <?php $this->endBody() ?>
-
-    <script>
-        if (window.matchMedia("(min-width: 768px)").matches) {
-            var dropdownItems = document.querySelectorAll('.navbar-nav .dropdown');
-
-            dropdownItems.forEach(function (item) {
-                item.addEventListener('mouseenter', function () {
-                    if (this.querySelector('.dropdown-toggle')) {
-                        this.querySelector('.dropdown-menu').classList.add('show');
-                    }
-                });
-
-                item.addEventListener('mouseleave', function () {
-                    this.querySelector('.dropdown-menu').classList.remove('show');
-                });
-            });
-        } else {
-            $('.navbar-nav .dropdown .dropdown-toggle').on('click', function (e) {
-                e.preventDefault();
-                e.stopPropagation();
-                $(this).siblings('.dropdown-menu').classList.add('show');
-            });
-
-            $(document).on('click', function (e) {
-                if ($('.navbar-nav').has(e.target).length === 0) {
-                    $('.dropdown-menu').classList.remove('show');
-                }
-            });
-        }
-    </script>
-
-
 
 </body>
 
---
+++ frontend/web/js/script.js
@@ -0,0 +1,28 @@
+// Dropdown do NavBar
+if (window.matchMedia("(min-width: 768px)").matches) {
+  var dropdownItems = document.querySelectorAll(".navbar-nav .dropdown");
+
+  dropdownItems.forEach(function (item) {
+    item.addEventListener("mouseenter", function () {
+      if (this.querySelector(".dropdown-toggle")) {
+        this.querySelector(".dropdown-menu").classList.add("show");
+      }
+    });
+
+    item.addEventListener("mouseleave", function () {
+      this.querySelector(".dropdown-menu").classList.remove("show");
+    });
+  });
+} else {
+  $(".navbar-nav .dropdown .dropdown-toggle").on("click", function (e) {
+    e.stopPropagation();
+    $(this).siblings(".dropdown-menu").classList.add("show");
+  });
+
+  $(document).on("click", function (e) {
+    if ($(".navbar-nav").has(e.target).length === 0) {
+      $(".dropdown-menu").classList.remove("show");
+    }
+  });
+}
+// Fim Dropdown
