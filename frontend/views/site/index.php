93571094489e869c888abdff46281d2655fc933e
Bruno Costa <brn.98@live.com>
2023-08-02 13:30:33 -0300

    Adição de acesso rápido e personalização no CSS

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,1 +1,77 @@
 <?= $this->render('/layouts/_carousel') ?>
+
+<div class="container text-center d-flex justify-content-center">
+    <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3 mt-n5 position-absolute mx-auto">
+        <div class="col">
+            <a href="#" class="text-decoration-none">
+                <div class="p-3 border bg-light rounded d-flex justify-content-evenly align-items-center"><i
+                        class="fas fa-calendar-check fa-2x text-warning"></i>Row
+                    column</div>
+            </a>
+        </div>
+        <div class="col">
+            <a href="#" class="text-decoration-none">
+                <div class="p-3 border bg-light rounded d-flex justify-content-evenly align-items-center"><i
+                        class="fas fa-calendar-check fa-2x text-warning"></i>Row
+                    column</div>
+            </a>
+        </div>
+        <div class="col">
+            <a href="#" class="text-decoration-none">
+                <div class="p-3 border bg-light rounded d-flex justify-content-evenly align-items-center"><i
+                        class="fas fa-calendar-check fa-2x text-warning"></i>Row
+                    column</div>
+            </a>
+        </div>
+        <div class="col">
+            <a href="#" class="text-decoration-none">
+                <div class="p-3 border bg-light rounded d-flex justify-content-evenly align-items-center"><i
+                        class="fas fa-calendar-check fa-2x text-warning"></i>Row
+                    column</div>
+            </a>
+        </div>
+        <div class="col">
+            <a href="#" class="text-decoration-none">
+                <div class="p-3 border bg-light rounded d-flex justify-content-evenly align-items-center"><i
+                        class="fas fa-calendar-check fa-2x text-warning"></i>Row
+                    column</div>
+            </a>
+        </div>
+        <div class="col">
+            <a href="#" class="text-decoration-none">
+                <div class="p-3 border bg-light rounded d-flex justify-content-evenly align-items-center"><i
+                        class="fas fa-calendar-check fa-2x text-warning"></i>Row
+                    column</div>
+            </a>
+        </div>
+        <div class="col d-none d-md-block">
+            <a href="#" class="text-decoration-none">
+                <div class="p-3 border bg-light rounded d-flex justify-content-evenly align-items-center"><i
+                        class="fas fa-calendar-check fa-2x text-warning"></i>Row
+                    column</div>
+            </a>
+        </div>
+        <div class="col d-none d-md-block">
+            <a href="#" class="text-decoration-none">
+                <div class="p-3 border bg-light rounded d-flex justify-content-evenly align-items-center"><i
+                        class="fas fa-calendar-check fa-2x text-warning"></i>Row
+                    column</div>
+            </a>
+        </div>
+        <div class="col d-none d-md-block">
+            <a href="#" class="text-decoration-none">
+                <div class="p-3 border bg-light rounded d-flex justify-content-evenly align-items-center"><i
+                        class="fas fa-calendar-check fa-2x text-warning"></i>Row
+                    column</div>
+            </a>
+        </div>
+        <div class="col d-none d-md-block">
+            <a href="#" class="text-decoration-none">
+                <div class="p-3 border bg-light rounded d-flex justify-content-evenly align-items-center"><i
+                        class="fas fa-calendar-check fa-2x text-warning"></i>Row
+                    column</div>
+            </a>
+        </div>
+    </div>
+</div>
+
